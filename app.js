85bd3d2d3ce8a5a6fe6d281d030b26f028192fa0
Prabhu_patel <yashpalpatel@acropolis.in>
2023-06-30 09:51:29 +0530

    first h

---
+++ app.js
@@ -0,0 +1,17 @@
+const express=require("express");
+const app=express();
+const port=process.env.port || 3000 ;
+app.listen(port);
+
+const cors=require("cors");
+app.use(cors());
+app.use(express.json());
+const bodyParser=require("body-parser");
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get("/", (req,res)=>{
+    res.json({"message":{"data": req.query}});
+})
+app.post("/", (req,res)=>{
+    res.json({"message":{"data": req.body}});
+})
